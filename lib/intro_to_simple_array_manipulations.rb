3ae4cf5494983dfa3a0f616071f1377e7f979389
pianoFF <fangfei1star@gmail.com>
2020-11-25 14:49:02 -0500

    progress

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,33 @@
+require "pry"
 def using_push (ary, string) 
   ary.push(string)
+end
+
+def using_unshift(ary, string)
+  ary.unshift(string)
+end
+
+def using_delete_at(ary, int)
+  ary.delete_at(int)
+end
+
+def using_delete(ary, str)
+  ary.delete(str)
+end
+
+def using_flatten(ary)
+  # binding.pry
+  ary.flatten
+end 
+
+def using_uniq(ary)
+  ary.uniq
+end
+
+def using_insert(ary, new_item)
+  ary.insert(4,new_item)
+end
+
+def using_concat(ary_1, ary_2)
+  ary_1.concat(ary_2)
 end
